402116a12a9106304836fa33bd52e02e588cdd47
Ben Regenspan <bregenspan@gizmodomedia.com>
2017-06-09 19:06:16 -0400

    Make it work; filter down to common English words

--- ngrams.py
+++ ngrams.py
@@ -1,18 +1,26 @@
 from nltk import FreqDist
+from nltk.corpus import words
 import csv
+import re
 
 """
 Stolen from https://codereview.stackexchange.com/a/105990
 """
 
+vocab = set(w.lower() for w in words.words())
+
 
 def get_words():
     """generate english words from the whole gutemberg corpus"""
 
-    with open('metalarchive.csv', 'r') as csvfile:
+    with open('MA-band-names_2017-06-09.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
+        print(csvfile)
         for row in reader:
-            yield row[0]
+            match_name_in_link = re.search('\'>(.*)</a>', row[1])
+            if match_name_in_link is not None:
+                print(match_name_in_link.group(1))
+                yield match_name_in_link.group(1).replace(' ', '').lower()
 
 
 def ngrams(N, word, strict=True):
@@ -21,14 +29,15 @@ def ngrams(N, word, strict=True):
     at the end of the word where P < N"""
 
     last = N - 1 if strict else 0
-    for i in xrange(len(word) - last):
-        yield word[i:i+N]
+    for i in range(len(word) - last):
+        if word[i:i+N] in vocab:
+            yield word[i:i+N]
 
-def m_most_common_ngram_chars(M=5, N=3):
+def m_most_common_ngram_chars(M=50, N=6):
     """gets the top M most common substrings of N characters in English words"""
-    f = FreqDist(ngram for ngram in ngrams(N, word) for word in get_words())
+    f = FreqDist(ngram for word in get_words() for ngram in ngrams(N, word))
     return f.most_common(M)
 
 if __name__ == "__main__":
     # Uses the default values M=5, N=3
-    print m_most_common_ngram_chars()
+    print(m_most_common_ngram_chars())
