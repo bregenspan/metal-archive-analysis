08cb2b39353e6106feb0ee1431baf90a6b726a45
Ben Regenspan <bregenspan@gizmodomedia.com>
2017-06-09 19:25:56 -0400

    Parameterize

--- ngrams.py
+++ ngrams.py
@@ -9,11 +9,17 @@ Stolen from https://codereview.stackexchange.com/a/105990
 
 vocab = set(w.lower() for w in words.words())
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Get ngrams from metal-archives.com scraped CSV')
+parser.add_argument('--size', default=10, type=int,
+                    help='Length of n-grams to search for')
+args = parser.parse_args()
+
 
 def get_words():
     with open('MA-band-names_2017-06-09.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
-        print(csvfile)
         for row in reader:
             match_name_in_link = re.search('\'>(.*)</a>', row[1])
             if match_name_in_link is not None:
@@ -30,13 +36,13 @@ def ngrams(N, word, strict=True):
         if word[i:i+N] in vocab:
             yield word[i:i+N]
 
-def m_most_common_ngram_chars(M=50, N=14):
+def m_most_common_ngram_chars(N, M=50):
     """gets the top M most common substrings of N characters in English words"""
     f = FreqDist(ngram for word in get_words() for ngram in ngrams(N, word))
     return f.most_common(M)
 
 if __name__ == "__main__":
-    seven_letter_ngrams = m_most_common_ngram_chars()
+    seven_letter_ngrams = m_most_common_ngram_chars(M=50, N=args.size)
     print("\n\n\n")
     for (word, count,) in seven_letter_ngrams:
         print("                {} ({})".format(word, count))
