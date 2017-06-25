9504030fcb874c29b65db61940425fdaa0b9fabe
Ben Regenspan <bregenspan@gmail.com>
2017-06-25 17:13:30 -0400

    Demo page with same word list passed to song composer and band list

--- visualization/band-list.js
+++ visualization/band-list.js
@@ -1,9 +1,6 @@
 /**
  * Band list component
  */
-
-const FIXTURE = ['Steel Prophet', 'False Prophet', 'Prophets of Doom', 'Messiah Prophet', 'Prophetic Age', 'Black Prophets', 'Pontius Prophet', 'Prophet of Disasters', 'Prophet', 'Satanic Prophets', 'High Prophet', 'Inverted Prophet', 'Sturmprophet', 'Prophet', "Prophet's Eye", 'Weeping Prophet', 'False Prophet', 'Prophet', 'Sleeping Prophet', 'Concrete Prophet', 'Neurosplit Prophet', 'Dark Prophet', 'Prophetic', 'Dark Prophets', 'Prophet', 'Prophets on Vacation', 'Prophet Fulfilled', 'Blood of the Prophets', 'Prophets of War', 'Prophetic Disclosure', 'The Prophet', 'Aeternus Prophet', 'Disaster Prophet', 'Blasphemia Prophetica', 'Black Tar Prophet', 'Slaughter the Prophets', 'Doomsday Prophet', 'Pale Prophet', 'Prophets of the Rising Dead', 'Prophets of Saturn', "The Prophet's Whisper", 'Prophetic Scourge', 'Prophet of the Void', 'The Fallen Prophets', 'Prophetess', 'Silent Prophet', 'Feast for a Prophet'];
-
 const BandList = function (bandList) {
   const searchTerm = 'Prophet';
   const highlightRegex = new RegExp(`(${searchTerm})`, 'i');
@@ -17,5 +14,3 @@ const BandList = function (bandList) {
   });
   return list;
 }
-
-document.body.appendChild(BandList(FIXTURE));
