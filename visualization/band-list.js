4a11af11f67a03c5caae4a2a15e240cc8ff37e37
Ben Regenspan <bregenspan@gmail.com>
2017-06-25 20:23:15 -0400

    Some styling

--- visualization/band-list.js
+++ visualization/band-list.js
@@ -2,29 +2,46 @@
  * Band list component
  */
 class BandList {
-  constructor(bandList) {
+  constructor (bandList) {
     const searchTerm = 'Prophet';
     const highlightRegex = new RegExp(`(${searchTerm})`, 'i');
     const list = document.createElement('ul');
     list.className = 'band-list';
-    const elements = bandList.map((bandName) => {
+    bandList.forEach((bandName) => {
       const el = document.createElement('li');
-      const highlightedBandName = bandName.replace(highlightRegex, '<em>$1</em>')
+      const highlightedBandName = bandName.replace(highlightRegex, '<em>$1</em>');
       el.innerHTML = highlightedBandName;
       list.appendChild(el);
     });
+
     this.el = list;
   }
 
-  highlightIndex(index) {
+  onMounted () {
+    this.offset = (window.innerHeight / 2) + this.el.firstChild.offsetHeight;
+    this.el.style.transform = `translateY(${this.offset}px)`;
+    window.setTimeout(() => {
+      this.el.classList.add('animate', 'visible');
+    });
+  }
+
+  highlightIndex (index) {
     this.clearHighlight();
     const newSelected = this.el.getElementsByTagName('li')[index];
     if (newSelected) {
       newSelected.classList.add('selected');
+      this.centerOn(newSelected);
     }
   }
 
-  clearHighlight() {
+  centerOn (itemEl) {
+    const style = window.getComputedStyle(itemEl);
+    const height = parseInt(style.marginTop, 10) + parseInt(style.marginBottom, 10) + itemEl.offsetHeight;
+    this.offset -= height;
+    this.el.style.transform = `translateY(${this.offset}px)`;
+  }
+
+  clearHighlight () {
     const selected = this.el.querySelector('.selected');
     if (selected) {
       selected.classList.remove('selected');
